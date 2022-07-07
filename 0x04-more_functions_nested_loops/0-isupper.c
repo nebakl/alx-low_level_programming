fb37782b2c12d8382bb0e7d92ab42a4ede37b1b4
Nebiyu Aklilu <neblak777@gmail.com>
2022-07-07 08:14:45 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if the input is an uppercase letter in ASCII
+ * @c: integer to check
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+}
