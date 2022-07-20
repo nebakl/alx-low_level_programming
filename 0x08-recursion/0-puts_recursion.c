10d6b85f4fbbf4e4e4025052300d2ad1f8450745
Nebiyu Aklilu <neblak777@gmail.com>
2022-07-20 04:17:56 -0700

    task 0 recur

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,8 +2,8 @@
 
 /**
  * _puts_recursion - prints a string followed by a new line
- * @s: string to be printed@s: string to be printed
- * Return String
+ * @s: string to be printed
+ * * Return String
  */
 
 void _puts_recursion(char *s)
