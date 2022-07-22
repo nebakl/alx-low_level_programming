8c20c5c31646c2cfa9916552b84eddee4fabf95d
Nebiyu Aklilu <neblak777@gmail.com>
2022-07-22 03:22:14 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+ * main - Prints the name of the program followed by a new line
+ * @argc: count of the arguments of the program
+ * @argv: array of argc
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc >= 1)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
