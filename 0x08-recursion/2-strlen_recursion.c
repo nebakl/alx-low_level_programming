c9e445c525b5f217728a5e58bedae9419d62049d
Nebiyu Aklilu <neblak777@gmail.com>
2022-07-25 03:56:56 -0700

    recursion 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,20 +2,17 @@
 
 /**
  * _strlen_recursion - prints the length of a string
- * @s: string
- * 
+ * @s: string 
  * Return: string length
  */
 
 int _strlen_recursion(char *s)
 {
 	int i = 0;
-
 	if (*s)
 	{
 		i++;
 		i += _strlen_recursion(s + 1);
 	}
-
 	return(i)
 }
